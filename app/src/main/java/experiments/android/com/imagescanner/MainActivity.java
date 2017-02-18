de9b27eac4ce5bc4d060a47b3c4a4b351e9d2cf8
spiderpig86 <slim679975@gmail.com>
2017-02-17 22:07:45 -0500

    Initial commit

---
+++ app/src/main/java/experiments/android/com/imagescanner/MainActivity.java
@@ -0,0 +1,13 @@
+package experiments.android.com.imagescanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
