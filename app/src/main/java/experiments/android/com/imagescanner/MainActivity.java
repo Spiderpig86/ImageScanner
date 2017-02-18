0997bda3ebf58f5c67d653f74645474ff5f16919
spiderpig86 <slim679975@gmail.com>
2017-02-18 02:02:18 -0500

    Not going anywhere

--- app/src/main/java/experiments/android/com/imagescanner/MainActivity.java
+++ app/src/main/java/experiments/android/com/imagescanner/MainActivity.java
@@ -1,20 +1,29 @@
 package experiments.android.com.imagescanner;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
     private String imageName = null;
     private static String imageFolderPath = null;
     private Uri fileUri = null;
+
+    public static final String TAG = "main";
+    final int REQUEST_WRITE_EXTERNAL_STORAGE=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +59,23 @@ public class MainActivity extends AppCompatActivity {
                 capture();
             }
         });
+
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String path = (String) v.getTag();
+
+                if (path != null) {
+
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_VIEW);
+                    Uri imgUri = Uri.parse("file://" + path);
+                    intent.setDataAndType(imgUri, "image/*");
+                    startActivity(intent);
+
+                }
+            }
+        });
     }
 
     /** Check if this device has a camera */
@@ -62,29 +91,49 @@ public class MainActivity extends AppCompatActivity {
 
     public void capture() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             root = Environment.getExternalStorageDirectory().toString() +
                     "/Image_Capture";
 
             // Creating folders for image
             imageFolderPath = root + "/temp";
-            File imagesFolder = new File(imageFolderPath);
-            imagesFolder.mkdirs();
+            //File imagesFolder = new File(imageFolderPath);
+            File imagesFolder = new File(Environment
+                    .getExternalStorageDirectory().getAbsolutePath() + "/temp");
+            if (!imagesFolder.mkdirs()) {
+                Toast.makeText(this, "Unable to make dir", Toast
+                        .LENGTH_SHORT).show();
+            }
 
             // Create our file for the image
             imageName = "temp.png";
 
-            File image = new File(imageFolderPath, imageName);
-            fileUri = Uri.fromFile(image;
-            mImageView.setTag(imageFolderPath + File.separator + imageName);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
 
-            // Start Camera intent
-            Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                    requestPermission(this);
 
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                    //Toast.makeText(this, "Unable to make dir2", Toast
+                    //.LENGTH_SHORT).show();
+
+                } else {
+                    File image = new File(imageFolderPath, imageName);
+                    fileUri = Uri.fromFile(image);
+                    Toast.makeText(this, fileUri.toString(), Toast.LENGTH_SHORT).show();
+                    mImageView.setTag(imageFolderPath + File.separator + imageName);
+
+                    // Start Camera intent
+                    Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+
+            }
+
         }
-    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent
@@ -92,12 +141,77 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // If the request is good
+            Bitmap bitmap = null;
 
+            try {
+                GetThumb getThumb = new GetThumb();
+                bitmap = getThumb.getThumbnail(fileUri, this);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            // Show the image on the image view.
+            mImageView.setImageBitmap(bitmap);
         } else {
             Toast.makeText(this, "Something went wrong...", Toast
                     .LENGTH_SHORT).show();
         }
 
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+    }
+
+    private void requestPermission(final Context context){
+        if(ActivityCompat.shouldShowRequestPermissionRationale((Activity)context,Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // For example if the user has previously denied the permission.
+
+            new AlertDialog.Builder(context)
+                    .setMessage("Need storage permission")
+                    .setPositiveButton("yes", new DialogInterface
+                            .OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions((Activity) context,
+                                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                    REQUEST_WRITE_EXTERNAL_STORAGE);
+                        }
+                    }).show();
+
+        }else {
+            // permission has not been granted yet. Request it directly.
+            ActivityCompat.requestPermissions((Activity)context,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_WRITE_EXTERNAL_STORAGE);
+        }
+    }
+
+    // Needed since Android introduced runtime permissions or something
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_WRITE_EXTERNAL_STORAGE: {
+                if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this,
+                            getResources().getString(R.string.permission_storage_success),
+                            Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(this,
+                            getResources().getString(R.string
+                                    .permission_storage_error),
+                            Toast.LENGTH_SHORT).show();
+                    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                }
+                return;
+            }
+        }
     }
 
 }
